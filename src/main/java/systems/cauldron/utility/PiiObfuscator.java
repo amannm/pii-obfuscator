e6bce6f0a4cb0d3090eef6904dc15c4bb99e0216
Amann Malik <amannmalik@gmail.com>
2018-09-13 23:55:12 -0500

    checkpoint

--- src/main/java/systems/cauldron/utility/PiiObfuscator.java
+++ src/main/java/systems/cauldron/utility/PiiObfuscator.java
@@ -1,14 +1,53 @@
 package systems.cauldron.utility;
 
-import java.util.List;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
-public class PiiObfuscator<T extends Enum<T>> {
+public class PiiObfuscator {
 
-    public void scan(RecordKeyScanner<T> scanner, List<String[]> lines) {
-        lines.forEach(scanner::scan);
+    private final Path path;
+    private final String delimiter;
+
+    public PiiObfuscator(Path path, String delimiter) {
+        this.path = path;
+        this.delimiter = delimiter;
     }
 
-    public void rewrite(RecordKeyRewriter rewriter, List<String[]> lines) {
-        lines.forEach(rewriter::rewrite);
+    public void scan(RecordKeyScanner scanner) throws IOException {
+        Files.lines(path, StandardCharsets.UTF_8)
+                .map(line -> line.split(delimiter, -1))
+                .forEach(scanner::scan);
+    }
+
+    public void rewrite(RecordKeyRewriter rewriter) throws IOException {
+        Path tempFile = null;
+        try {
+            tempFile = Files.createTempFile(null, null);
+            try (BufferedWriter writer = Files.newBufferedWriter(tempFile, StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
+                Files.lines(path, StandardCharsets.UTF_8)
+                        .map(line -> line.split(delimiter, -1))
+                        .peek(rewriter::rewrite)
+                        .map(fields -> String.join(delimiter, fields))
+                        .forEach(str -> {
+                            try {
+                                writer.write(str);
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+            }
+        } finally {
+            if (tempFile != null) {
+                try {
+                    Files.delete(tempFile);
+                } catch (IOException ignored) {
+                }
+            }
+        }
+
     }
 }
--- src/main/java/systems/cauldron/utility/RecordKeyScanner.java
+++ src/main/java/systems/cauldron/utility/RecordKeyScanner.java
@@ -5,10 +5,11 @@ import java.util.*;
 public class RecordKeyScanner<T extends Enum<T>> {
 
     private final Map<T, Set<String>> keysets;
-    private final Map<Integer, Set<String>> collectors = new HashMap<>();
+    private final Map<Integer, Set<String>> collectors;
 
     public RecordKeyScanner(Class<T> clazz) {
         keysets = new EnumMap<>(clazz);
+        collectors = new HashMap<>();
     }
 
     public void setKeyColumn(int index, T type) {
