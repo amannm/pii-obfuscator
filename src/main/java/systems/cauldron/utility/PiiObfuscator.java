da95cb3670f127ac3cd70f0c5d9a93ac4bd4614b
Amann Malik <amannmalik@gmail.com>
2018-09-13 23:38:20 -0500

    lol

--- src/main/java/systems/cauldron/utility/PiiObfuscator.java
+++ src/main/java/systems/cauldron/utility/PiiObfuscator.java
@@ -1,14 +1,11 @@
 package systems.cauldron.utility;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class PiiObfuscator<T extends Enum<T>> {
 
-    public Map<T, Set<String>> scan(RecordKeyScanner<T> scanner, List<String[]> lines) {
+    public void scan(RecordKeyScanner<T> scanner, List<String[]> lines) {
         lines.forEach(scanner::scan);
-        return scanner.getResults();
     }
 
     public void rewrite(RecordKeyRewriter rewriter, List<String[]> lines) {
