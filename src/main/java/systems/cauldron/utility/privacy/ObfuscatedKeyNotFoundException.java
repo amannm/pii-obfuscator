d5dbdbe82ab45047f558d431726eae7fa26bfcc7
Amann Malik <amannmalik@gmail.com>
2018-09-15 19:06:05 -0500

    define superclass validation

--- src/main/java/systems/cauldron/utility/privacy/ObfuscatedKeyNotFoundException.java
+++ src/main/java/systems/cauldron/utility/privacy/ObfuscatedKeyNotFoundException.java
@@ -4,4 +4,8 @@ public class ObfuscatedKeyNotFoundException extends RuntimeException {
     public ObfuscatedKeyNotFoundException(String scannedKey) {
         super("no obfuscated key found for scanned key: " + scannedKey);
     }
+
+    public ObfuscatedKeyNotFoundException() {
+        super("no obfuscated key mismatch");
+    }
 }
--- src/main/java/systems/cauldron/utility/privacy/RemoteKeyTransformer.java
+++ src/main/java/systems/cauldron/utility/privacy/RemoteKeyTransformer.java
@@ -14,7 +14,16 @@ public abstract class RemoteKeyTransformer<T extends Enum<T>> extends KeyTransfo
     public Map<T, Map<String, String>> generateScannedKeymaps(RecordKeyScanner<T> scanner) {
         return scanner.getResults().entrySet().stream().collect(Collectors.toMap(
                 Map.Entry::getKey,
-                e -> mapScannedKeys(e.getKey(), e.getValue())));
+                e -> {
+                    Set<String> scannedKeys = e.getValue();
+                    Map<String, String> keyMap = mapScannedKeys(e.getKey(), scannedKeys);
+                    Set<String> keyMapSet = keyMap.keySet();
+                    if (!keyMapSet.containsAll(scannedKeys)) {
+                        throw new ObfuscatedKeyNotFoundException();
+                    }
+                    keyMapSet.retainAll(scannedKeys);
+                    return keyMap;
+                }));
     }
 
     abstract Map<String, String> mapScannedKeys(T type, Set<String> scannedKeys);
