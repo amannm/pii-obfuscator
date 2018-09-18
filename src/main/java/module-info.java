d99a0fcd4dbcadca78a31b658996cb307de668b8
Amann Malik <amannmalik@gmail.com>
2018-09-17 22:28:49 -0500

    hah

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,5 @@ module systems.cauldron.utility.privacy {
     exports systems.cauldron.utility.privacy;
     exports systems.cauldron.utility.privacy.mapper;
     exports systems.cauldron.utility.privacy.operations;
+    exports systems.cauldron.utility.privacy.exception;
 }
